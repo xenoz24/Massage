81d5bd92428d5ec302e057a99452e630ab305c7d
xenoz24 <46815590+xenoz24@users.noreply.github.com>
2019-04-02 19:32:27 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,30 +1,41 @@
-
+import time
 import socket
+import server
+import threading
 
 s = None
 host = None
 port = None
+clientname = None
 def socket_set():
     global s,host,port
-    s = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostname()
     port = 9999
     s.connect((host, port))
-def con():
-    while True:
-        sendmsg = input('send:')
 
-        if sendmsg == 'exit':
-            s.close()
-            break
-        info = s.send(sendmsg.encode())
+def recive():
+    while True:
         msg = s.recv(1024)
         if not msg:
             pass
         else:
-            print(msg.decode('utf-8'))
+            print('伺服器: %s' % msg.decode('utf-8') + '\n')
+
+def send():
+    clientname = 'aa'
+
+    while 1:
+        sendmsg = input('%s send:' % clientname)
+        if sendmsg == 'exit':
+            s.close()
+            break
+        s.send(sendmsg.encode())
+
 
 if __name__ == '__main__':
     socket_set()
-    con()
+    send_thread = threading.Thread(target=send)
+    send_thread.start()
+    recive_thread = threading.Thread(target=recive)
+    recive_thread.start()
